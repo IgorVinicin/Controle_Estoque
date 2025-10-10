6d4658ea87076730cebaa1f50e8e28d9969e193d
IgorVinicin <iaraujopecedossantos@gmail.com>
2025-10-10 16:58:47 -0300

    adicionando função nos botões

---
+++ Dashboard/Dashboard/CadastrarProd.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Dashboard
+{
+    partial class CadastrarProd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPrecoCusto = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtPrecoVenda = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEstoqProd = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCategoriaProd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCadastrarProd = new System.Windows.Forms.Button();
+            this.txtQuantProd = new System.Windows.Forms.TextBox();
+            this.txtNomeProd = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btnCancelar);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.txtPrecoCusto);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.txtPrecoVenda);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.txtEstoqProd);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.txtCategoriaProd);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.btnCadastrarProd);
+            this.panel2.Controls.Add(this.txtQuantProd);
+            this.panel2.Controls.Add(this.txtNomeProd);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(589, 450);
+            this.panel2.TabIndex = 7;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.White;
+            this.btnCancelar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancelar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(323, 325);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 41);
+            this.btnCancelar.TabIndex = 18;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(99, 259);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(129, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Preço de Custo (R$)";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtPrecoCusto
+            // 
+            this.txtPrecoCusto.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtPrecoCusto.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPrecoCusto.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.txtPrecoCusto.Location = new System.Drawing.Point(103, 279);
+            this.txtPrecoCusto.Multiline = true;
+            this.txtPrecoCusto.Name = "txtPrecoCusto";
+            this.txtPrecoCusto.Size = new System.Drawing.Size(167, 29);
+            this.txtPrecoCusto.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(320, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(128, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Preço de Venda(R$)";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtPrecoVenda
+            // 
+            this.txtPrecoVenda.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtPrecoVenda.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPrecoVenda.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.txtPrecoVenda.Location = new System.Drawing.Point(323, 279);
+            this.txtPrecoVenda.Multiline = true;
+            this.txtPrecoVenda.Name = "txtPrecoVenda";
+            this.txtPrecoVenda.Size = new System.Drawing.Size(167, 29);
+            this.txtPrecoVenda.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(320, 189);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 17);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Estoque Minimo";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtEstoqProd
+            // 
+            this.txtEstoqProd.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtEstoqProd.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEstoqProd.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.txtEstoqProd.Location = new System.Drawing.Point(323, 209);
+            this.txtEstoqProd.Multiline = true;
+            this.txtEstoqProd.Name = "txtEstoqProd";
+            this.txtEstoqProd.Size = new System.Drawing.Size(167, 29);
+            this.txtEstoqProd.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(99, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Quantidade";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtCategoriaProd
+            // 
+            this.txtCategoriaProd.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtCategoriaProd.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtCategoriaProd.Location = new System.Drawing.Point(102, 151);
+            this.txtCategoriaProd.Multiline = true;
+            this.txtCategoriaProd.Name = "txtCategoriaProd";
+            this.txtCategoriaProd.Size = new System.Drawing.Size(388, 26);
+            this.txtCategoriaProd.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(100, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Categoria";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(99, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nome do Produto";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCadastrarProd
+            // 
+            this.btnCadastrarProd.BackColor = System.Drawing.Color.Black;
+            this.btnCadastrarProd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCadastrarProd.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrarProd.ForeColor = System.Drawing.Color.White;
+            this.btnCadastrarProd.Location = new System.Drawing.Point(415, 325);
+            this.btnCadastrarProd.Name = "btnCadastrarProd";
+            this.btnCadastrarProd.Size = new System.Drawing.Size(75, 41);
+            this.btnCadastrarProd.TabIndex = 8;
+            this.btnCadastrarProd.Text = "Cadastrar";
+            this.btnCadastrarProd.UseVisualStyleBackColor = false;
+            this.btnCadastrarProd.Click += new System.EventHandler(this.btnCadastrarProd_Click);
+            // 
+            // txtQuantProd
+            // 
+            this.txtQuantProd.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtQuantProd.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtQuantProd.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.txtQuantProd.Location = new System.Drawing.Point(103, 209);
+            this.txtQuantProd.Multiline = true;
+            this.txtQuantProd.Name = "txtQuantProd";
+            this.txtQuantProd.Size = new System.Drawing.Size(167, 29);
+            this.txtQuantProd.TabIndex = 3;
+            // 
+            // txtNomeProd
+            // 
+            this.txtNomeProd.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txtNomeProd.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtNomeProd.Location = new System.Drawing.Point(103, 96);
+            this.txtNomeProd.Multiline = true;
+            this.txtNomeProd.Name = "txtNomeProd";
+            this.txtNomeProd.Size = new System.Drawing.Size(388, 26);
+            this.txtNomeProd.TabIndex = 0;
+            // 
+            // CadastrarProd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 450);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CadastrarProd";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CadastrarProd";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPrecoCusto;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtPrecoVenda;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEstoqProd;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCategoriaProd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCadastrarProd;
+        private System.Windows.Forms.TextBox txtQuantProd;
+        private System.Windows.Forms.TextBox txtNomeProd;
+    }
+}
---
+++ Dashboard/Dashboard/CadastrarProd.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace Dashboard
+{
+    public partial class CadastrarProd : Form
+    {
+        public CadastrarProd()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            frmDashBoard dashboard = new frmDashBoard();
+            dashboard.Show();
+            this.Close();
+        }
+
+        private void btnCadastrarProd_Click(object sender, EventArgs e)
+        {
+            string nmProduto = "";
+            string categoria = "";
+            int quantidade = 0;
+            int estoquemin = 0;
+            decimal precoCusto = 0;
+            decimal precoVenda = 0;
+
+
+
+            nmProduto= txtNomeProd.Text;
+            categoria = txtCategoriaProd.Text;
+            quantidade = int.Parse(txtQuantProd.Text);
+            estoquemin = int.Parse(txtEstoqProd.Text);
+            precoCusto = decimal.Parse(txtPrecoCusto.Text);
+            precoVenda = decimal.Parse(txtPrecoVenda.Text);
+
+
+            try
+            {
+                string conexaoString = "server=localhost;user=root;password=;database=ChrisCell";
+                using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+                    {
+                    conexao.Open();
+                    string query = "INSERT INTO produtos (NomeProduto, Categoria, Quantidade, EstoqueMinimo, PrecoCusto, PrecoVenda) " +
+                                   "VALUES (@NomeProduto, @Categoria, @Quantidade, @EstoqueMinimo, @PrecoCusto, @PrecoVenda)";
+                    using (MySqlCommand comando = new MySqlCommand(query, conexao))
+                    {
+                        comando.Parameters.AddWithValue("@NomeProduto", nmProduto);
+                        comando.Parameters.AddWithValue("@Categoria", categoria);
+                        comando.Parameters.AddWithValue("@Quantidade", quantidade);
+                        comando.Parameters.AddWithValue("@EstoqueMinimo", estoquemin);
+                        comando.Parameters.AddWithValue("@PrecoCusto", precoCusto);
+                        comando.Parameters.AddWithValue("@PrecoVenda", precoVenda);
+                        int linhasAfetadas = comando.ExecuteNonQuery();
+                        if (linhasAfetadas > 0)
+                        {
+                            MessageBox.Show("Produto cadastrado com sucesso!");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Falha ao cadastrar o produto.");
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao conectar ao banco de dados: " + ex.Message);
+            }
+
+
+
+        }
+    }
+}
